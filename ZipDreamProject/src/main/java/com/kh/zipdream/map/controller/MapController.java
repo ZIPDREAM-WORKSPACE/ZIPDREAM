cc16211de94a5822400b479b35ec2dfdf2bc57f6
hongdii <93081185+hongdii@users.noreply.github.com>
2023-05-22 22:00:36 +0900

    0522

--- ZipDreamProject/src/main/java/com/kh/zipdream/map/controller/MapController.java
+++ ZipDreamProject/src/main/java/com/kh/zipdream/map/controller/MapController.java
@@ -1,16 +1,43 @@
 package com.kh.zipdream.map.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.google.gson.Gson;
+import com.kh.zipdream.map.model.service.MapService;
 
 @Controller
 @RequestMapping("/map")
 public class MapController {
 	
+	private MapService mapService;
+	
+	@Autowired
+	public MapController(MapService mapService) {
+		this.mapService = mapService;
+	}
+	
+	public MapController() {
+		
+	}
+	
 	@GetMapping("/main")
 	public String moveMapController(){
 		return "map/mapFirstPage";
+	}
+	
+	@GetMapping("/jqAutoSearch")
+	public String autoSearch(String keyword) {
+		
+		ArrayList<??> list = mapService.selectKeywordList(keyword);
+		
+		
+		return new Gson().toJson(list);
 	}
 	
 }
