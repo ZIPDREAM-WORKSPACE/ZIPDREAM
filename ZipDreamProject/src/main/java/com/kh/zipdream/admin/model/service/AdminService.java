ed1c416dd7838ede99698ac5ea95bf16e117e8a4
JunHaRyu <wnsgk1207@gmail.com>
2023-05-22 10:31:21 +0900

    update

--- ZipDreamProject/src/main/java/com/kh/zipdream/admin/model/service/AdminService.java
+++ ZipDreamProject/src/main/java/com/kh/zipdream/admin/model/service/AdminService.java
@@ -1,8 +1,7 @@
 package com.kh.zipdream.admin.model.service;
 
+import java.util.List;
 import java.util.Map;
-
-import com.kh.zipdream.admin.model.vo.MemberApply;
 
 public interface AdminService {
 	
@@ -12,6 +11,6 @@ public interface AdminService {
 	public Map<String,Object> countReport();
 	public Map<String,Object> countEvent();
 	public Map<String,Object> countChattingRoom();
-	public Map<String, MemberApply> selectApplyListLimit5();
+	public List<Map<String,String>> selectApplyListLimit5();
 	
 }
