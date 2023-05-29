8671a3d04b53d08daa017d740320cf77877be710
eeyj <117423276+eeyj@users.noreply.github.com>
2023-05-29 19:44:25 +0900

    5/29
    분양일정_api ajax 작업

--- ZipDreamProject/src/main/java/com/kh/zipdream/sales/controller/SalesController.java
+++ ZipDreamProject/src/main/java/com/kh/zipdream/sales/controller/SalesController.java
@@ -1,8 +1,17 @@
 package com.kh.zipdream.sales.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller 
 @RequestMapping("/sales")
@@ -26,6 +35,80 @@ public class SalesController {
 	@GetMapping("/faq")
 	public String moveSalesFaq() {
 		return "sales/salesFaq";
+	}
+	
+	@ResponseBody
+	@GetMapping("/apiData")
+	@PostMapping("/apiData")
+	@RequestMapping(value="/apiData",produces="application/json; charset=UTF-8")
+	public String getAPTLttotPblancDetail() {
+		
+		StringBuffer result = new StringBuffer();
+		
+		String apiUrl = "https://api.odcloud.kr/api/ApplyhomeInfoDetailSvc/v1/getAPTLttotPblancDetail?page=1&perPage=10&serviceKey=29yT0hzwPhkcH3v%2FVb1TblTy2MScYEvVCkJPq98BUa2T4VbppSCan3zrhXE8Gz%2BBagEwWPPiwfPMWcP0WSu9vg%3D%3D";
+		
+		URL url;
+		try {
+			url = new URL(apiUrl);
+			HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
+			urlconnection.setRequestMethod("GET");
+			urlconnection.setRequestProperty("Content-Type", "application/json; utf-8"); //post body json으로 던지기 위함
+			urlconnection.setRequestProperty("Accept", "application/json");
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(),"UTF-8"));
+			
+			String returnLine;
+			
+			while((returnLine = br.readLine()) != null) {
+				result.append(returnLine);
+				/* System.out.println(br.readLine()); */
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		 return result.toString();
+	}
+	
+	
+
+	@ResponseBody
+	@GetMapping("/houseInfo")
+	@PostMapping("/houseInfo")
+	@RequestMapping(value="/houseInfo",produces="application/json; charset=UTF-8")
+	public String getAPTLttotPblancMdl(String houseCode) {
+		
+		StringBuffer result = new StringBuffer();
+		
+		String apiUrl = "https://api.odcloud.kr/api/ApplyhomeInfoDetailSvc/v1/getAPTLttotPblancMdl?page=1&perPage=10&cond%5BHOUSE_MANAGE_NO%3A%3AEQ%5D=";
+		//이건 내 서비스인증키
+		String serviceKey = "29yT0hzwPhkcH3v%2FVb1TblTy2MScYEvVCkJPq98BUa2T4VbppSCan3zrhXE8Gz%2BBagEwWPPiwfPMWcP0WSu9vg%3D%3D";
+//		2020000001&serviceKey=29yT0hzwPhkcH3v%2FVb1TblTy2MScYEvVCkJPq98BUa2T4VbppSCan3zrhXE8Gz%2BBagEwWPPiwfPMWcP0WSu9vg%3D%3D
+		URL url;
+		try {
+			url = new URL(apiUrl+houseCode+"&serviceKey="+serviceKey);
+			HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
+			urlconnection.setRequestMethod("GET");
+			urlconnection.setRequestProperty("Content-Type", "application/json; utf-8"); //post body json으로 던지기 위함
+			urlconnection.setRequestProperty("Accept", "application/json");
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(),"UTF-8"));
+			
+			String returnLine;
+			
+			while((returnLine = br.readLine()) != null) {
+				result.append(returnLine);
+				/* System.out.println(br.readLine()); */
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		 return result.toString();
 	}
 
 }
