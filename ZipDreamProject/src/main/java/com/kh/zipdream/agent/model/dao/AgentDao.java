e705bccf6c2bdb6b79e655d402510f225bf30a4d
eeyj <117423276+eeyj@users.noreply.github.com>
2023-06-10 18:47:45 +0900

    6/10
    최근본방, 공인중개사페이지 신청매물 작업

---
+++ ZipDreamProject/src/main/java/com/kh/zipdream/agent/model/dao/AgentDao.java
@@ -0,0 +1,35 @@
+package com.kh.zipdream.agent.model.dao;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.zipdream.common.model.vo.PageInfo;
+import com.kh.zipdream.mypage.model.vo.MyRoomSell;
+
+@Repository
+public class AgentDao {
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	public int selectRequedstListCount(int userNo) {
+		
+		return sqlSession.selectOne("agent-mapper.selectRequestListCount", userNo);
+	}
+	
+	public ArrayList<MyRoomSell> selectRequestList(PageInfo pi,int userNo){
+		
+		int offset = (pi.getCurrentPage()-1) * pi.getBoardLimit();
+		int limit = pi.getBoardLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, limit);
+		
+		return (ArrayList)sqlSession.selectList("agent-mapper.selectRequestList", userNo, rowBounds);
+		
+	}
+	
+}
